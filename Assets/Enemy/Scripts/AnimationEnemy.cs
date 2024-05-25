3e81ea790b16b82facdd624db6df0ca6f54ba4e3
VasiliIgnatenko <30416299+VasiliIgnatenko@users.noreply.github.com>
2024-05-25 16:33:09 +0300

    add anim run for enemy

--- Assets/Enemy/Scripts/AnimationEnemy.cs
+++ Assets/Enemy/Scripts/AnimationEnemy.cs
@@ -1,7 +1,22 @@
+using UnityEngine;
+
 namespace EnemySpace
 {
-    public class AnimationEnemy
+    public class AnimationEnemy : MonoBehaviour
     {
+        private Animator _animator;
+        private Enemy _enemy;
         
+        void Start()
+        {
+            _enemy = GetComponent<EnemySpace.Enemy>();
+            _animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update() 
+        {
+            _animator.SetBool("isMove", _enemy.IsMoving);
+        }
     }
 }
